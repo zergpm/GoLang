3b0f49d6052aa8a04d06be3653a4ee0c07682f3f
zergpm <zergpm@gmail.com>
2021-11-18 00:50:19 +0200

    add serv_8080

---
+++ main_file.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func home_page(page http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(page, "Bla Bla Bla")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	handleRequest()
+}
